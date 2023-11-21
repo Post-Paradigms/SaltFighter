4c753e78f1a2e320fbeb535274e549939210a139
Davie Nguyen <davie19nguyen@gmail.com>
2023-11-20 22:33:48 -0600

    fixed controller creation bug

--- Source/FGTest/FightGameMode.cpp
+++ Source/FGTest/FightGameMode.cpp
@@ -28,21 +28,26 @@ void AFightGameMode::BeginPlay()
     for (int i = 0; i < PlayerStarts.Num(); ++i)
     {
         if (i == 0) { // Setup player 1
-            P1FighterController = Cast<AFighterController>(UGameplayStatics::CreatePlayer(GetWorld()));
             if (FighterBPClass)
                 P1FighterCharacter = GetWorld()->SpawnActor<AFighter>(FighterBPClass, PlayerStarts[i]->GetActorTransform());
+
+            if (GetWorld()->GetNumPlayerControllers() == 0)
+                UGameplayStatics::CreatePlayer(GetWorld(), 0);
+            P1FighterController = Cast<AFighterController>(UGameplayStatics::GetPlayerControllerFromID(GetWorld(), 0));
+
             if (P1FighterController && P1FighterCharacter)
                 P1FighterController->Possess(P1FighterCharacter);
         }
         else if (i == 1) { // Setup player 2
-            P2FighterController = Cast<AFighterController>(UGameplayStatics::CreatePlayer(GetWorld()));
             if (FighterBPClass)
                 P2FighterCharacter = GetWorld()->SpawnActor<AFighter>(FighterBPClass, PlayerStarts[i]->GetActorTransform());
+
+            if (GetWorld()->GetNumPlayerControllers() == 1)
+                UGameplayStatics::CreatePlayer(GetWorld(), 1);
+            P2FighterController = Cast<AFighterController>(UGameplayStatics::GetPlayerControllerFromID(GetWorld(), 1));
+
             if (P2FighterController && P2FighterCharacter)
                 P2FighterController->Possess(P2FighterCharacter);
-        }
-        else {
-            // umm, need i explain?
         }
     }
 
@@ -55,17 +60,13 @@ void AFightGameMode::BeginPlay()
         P2FighterCharacter->OtherPlayer = P1FighterCharacter;
         P2FighterCharacter->OurController = P2FighterController;
     }
-    else {
-        // Something is wrong!!!!
-    }
 
     // Create HUD
-    if (FightingHUDClass)
+    if (FightingHUDClass && P1FighterController)
     {
         FightingHUD = CreateWidget<UFightingHUDUserWidget>(P1FighterController, FightingHUDClass);
         FightingHUD->AddToPlayerScreen();
     }
-
 }
 
 void AFightGameMode::EndPlay(const EEndPlayReason::Type EndPlayReason)
@@ -78,5 +79,4 @@ void AFightGameMode::EndPlay(const EEndPlayReason::Type EndPlayReason)
         FightingHUD->RemoveFromParent();
         FightingHUD = nullptr;
     }
-
 }
