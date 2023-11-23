a66e73b7ea19ca833c062dafae0389ab0e4235bc
Kaiden-z <kaidenzapanta@utexas.edu>
2023-11-23 14:08:23 -0600

    fixed compile error

--- Source/FGTest/Fighter.h
+++ Source/FGTest/Fighter.h
@@ -146,10 +146,6 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Hurtbox")
 	class UHurtbox * HurtboxComponent;
 
-	// hurtbox component 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Hurtbox")
-	class UHurtbox * HurtboxComponent;
-
 private:
 	// Camera Components
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
