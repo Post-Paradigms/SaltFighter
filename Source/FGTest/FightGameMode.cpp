95ad1e3bb7761c80de2aede648d16c88b0d55a48
lekenneth77 <lekenneth77@gmail.com>
2023-12-09 18:07:24 -0600

    oops forgot to add back the intro

--- Source/FGTest/FightGameMode.cpp
+++ Source/FGTest/FightGameMode.cpp
@@ -113,16 +113,17 @@ void AFightGameMode::BeginPlayPlay() {
     ASharedCamera* TheCamera = Cast<ASharedCamera>(Camera[0]);
     TheCamera->ActivateCamera();
 
- /*   StartRoundCountdownTimerCount = 0;
+    StartRoundCountdownTimerCount = 0;
     FightingHUD->UpdateCountdown(FText::FromString("TURN UP THE HEAT"));
     UGameplayStatics::PlaySound2D(GetWorld(), tuth, 3.0f, 1.0f, 0.5f);
     GetWorld()->GetTimerManager().SetTimer(StartRoundCountdownTimer, this, &AFightGameMode::StartRoundCountdown, 2.0f, true);
-    FightingHUD->StartRoundTimer();*/
+    FightingHUD->StartRoundTimer();
 
-    P1FighterController->EnableInput(P1FighterController);
+    
+    /*P1FighterController->EnableInput(P1FighterController);
     P2FighterController->EnableInput(P2FighterController);
     GetWorld()->GetTimerManager().ClearTimer(StartRoundCountdownTimer);
-    StartRoundCountdownTimerCount = 0;
+    StartRoundCountdownTimerCount = 0;*/
 }
 
 
